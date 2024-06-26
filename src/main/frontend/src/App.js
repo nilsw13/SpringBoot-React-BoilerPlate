9d5cf73a2a22b3c85acf41c84057a2517a8a55e3
nilsw13 <nils.wenting@laplateforme.io>
2024-06-26 13:17:06 +0200

    change logo

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <img src="/developpement-de-logiciels.png" className="App-logo" alt="logo" />
+          <img src="/codage.png" className="App-logo" alt="logo" />
           <h1>Shit here we go again..</h1>
           <p>good luck for this new project Nilso..<img className="trefle" src="trefle.png" alt=""></img>
           </p>
